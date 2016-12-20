b45b1c0b15228d9dbd198fb04e930fc45c2d6da7
get memed :^) <Space Raiders@Team2537-Zenith>
2016-12-19 19:08:13 -0500

    :^)

--- src/org/usfirst/frc/team2537/robot/Robot.java
+++ src/org/usfirst/frc/team2537/robot/Robot.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+//Hello
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
