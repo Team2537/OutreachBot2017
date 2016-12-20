648411318348cd3c64086f41c16cedd93e21b027
get memed :^) <Space Raiders@Team2537-Zenith>
2016-12-19 19:56:39 -0500

    Fixed and streamlined limit switch code

--- src/org/usfirst/frc/team2537/robot/drive/DriveLimitSwitchCommand.java
+++ src/org/usfirst/frc/team2537/robot/drive/DriveLimitSwitchCommand.java
@@ -10,31 +10,24 @@ public class DriveLimitSwitchCommand extends Command {
 	protected void initialize() {
 		Robot.driveSys.setLeftMotor(.2);
 		Robot.driveSys.setRightMotor(.2);
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected boolean isFinished() {
-		if (Robot.driveSys.limitSwitch.get() == true) {
-			return true;
-		} else {
-			return false;
-		}
-		// TODO Auto-generated method stub
+		return Robot.driveSys.limitSwitch.get();
+
 	}
 
 	@Override
 	protected void end() {
 		Robot.driveSys.setLeftMotor(0);
 		Robot.driveSys.setRightMotor(0);
-		// TODO Auto-generated method stub
 
 	}
 
@@ -42,7 +35,6 @@ public class DriveLimitSwitchCommand extends Command {
 	protected void interrupted() {
 		Robot.driveSys.setLeftMotor(0);
 		Robot.driveSys.setRightMotor(0);
-		// TODO Auto-generated method stub
 
 	}
 
