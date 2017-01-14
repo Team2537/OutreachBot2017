34bf37faa818e4a0a36c47e0bc660b840b8dbe4a
Daniel Osheroff <email2daniel2@gmail.com>
2017-01-13 21:28:47 -0500

    Finished activation

--- src/org/usfirst/frc/team2537/robot/drive/DriveSubsystem.java
+++ src/org/usfirst/frc/team2537/robot/drive/DriveSubsystem.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team2537.robot.drive;
 
 import org.usfirst.frc.team2537.robot.Ports;
+import org.usfirst.frc.team2537.robot.climber.ClimberCommand;
 import org.usfirst.frc.team2537.robot.input.HumanInput;
 
 import edu.wpi.first.wpilibj.DigitalInput;
@@ -28,7 +29,7 @@ public class DriveSubsystem extends Subsystem {
 	}
 
 	public void registerButtons() {
-		
+		HumanInput.registerWhenPressedCommand(HumanInput.climberActivateButton, new ClimberCommand());
 	}
 
 	/**
