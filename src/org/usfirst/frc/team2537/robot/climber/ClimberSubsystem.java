998edcfa218a8b02bb610256de3c722eb0908e14
Daniel Osheroff <email2daniel2@gmail.com>
2017-01-15 11:33:14 -0500

    Switched Talon to CANTslon

--- src/org/usfirst/frc/team2537/robot/climber/ClimberSubsystem.java
+++ src/org/usfirst/frc/team2537/robot/climber/ClimberSubsystem.java
@@ -3,13 +3,15 @@ package org.usfirst.frc.team2537.robot.climber;
 import org.usfirst.frc.team2537.robot.Ports;
 import org.usfirst.frc.team2537.robot.input.HumanInput;
 
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class ClimberSubsystem extends Subsystem {
 
-	private Talon climberMotor = new Talon(Ports.CLIMBER_MOTOR); //creates Talon motor for climber
+	private CANTalon climberMotor = new CANTalon(Ports.CLIMBER_MOTOR); //creates Talon motor for climber
 	protected static final double SPEED_MULTIPLIER = 1;
 	
 	DigitalInput limitSwitch = new DigitalInput(Ports.LIMIT_SWITCH_BUTTON);
