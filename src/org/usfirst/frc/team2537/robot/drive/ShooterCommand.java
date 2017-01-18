ad9de8c6b52473d675d2503a26bf84d644434a0f
karan <karan@KaranLuthriaLinux>
2017-01-17 19:46:54 -0500

    Alex likes javadoc

--- src/org/usfirst/frc/team2537/robot/drive/ShooterCommand.java
+++ src/org/usfirst/frc/team2537/robot/drive/ShooterCommand.java
@@ -6,25 +6,39 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class ShooterCommand extends Command {
 	private boolean shooterOn;
-	public ShooterCommand(boolean b) {
+
+	/**
+	 * Constructor for Command Requiring Shooter
+	 * 
+	 * @param shooterOn
+	 *            - if Fly Wheels are off or on
+	 */
+	public ShooterCommand(boolean shooterOn) {
 		requires(Robot.shooter);
-		this.shooterOn = b;
+		this.shooterOn = shooterOn;
 	}
 
 	@Override
+	/**
+	 * Ends by Turing FlyWheels Off
+	 */
 	protected void end() {
 		// TODO Auto-generated method stub
 		Robot.shooter.FlyOff();
 	}
 
+	/**
+	 * if the robot is in range And shooterOn = true - Flywheels to speed 1
+	 */
 	@Override
 	protected void execute() {
 		// TODO Auto-generated method stub
-	if(shooterOn){	
-		if (Robot.shooter.UltronRange() > ShooterSubsystem.DISTANCE_TO_BOILER - ShooterSubsystem.LEEWAY
-				&& Robot.shooter.UltronRange() < ShooterSubsystem.DISTANCE_TO_BOILER + ShooterSubsystem.LEEWAY){
-			Robot.shooter.FlyOn();
-		}}else{
+		if (shooterOn) {
+			if (Robot.shooter.UltronRange() > ShooterSubsystem.DISTANCE_TO_BOILER - ShooterSubsystem.LEEWAY
+					&& Robot.shooter.UltronRange() < ShooterSubsystem.DISTANCE_TO_BOILER + ShooterSubsystem.LEEWAY) {
+				Robot.shooter.FlyOn();
+			}
+		} else {
 			Robot.shooter.FlyOff();
 		}
 	}
@@ -36,19 +50,21 @@ public class ShooterCommand extends Command {
 	}
 
 	@Override
+	/**
+	 * Turns flywheels off when interrupted
+	 */
 	protected void interrupted() {
 		// TODO Auto-generated method stub
 		Robot.shooter.FlyOff();
 	}
 
 	@Override
+	/**
+	 * if Robot is out of range or shooterOn is not true - Stop Running
+	 */
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
-	return !shooterOn || Robot.shooter.UltronRange() < Robot.shooter.DISTANCE_TO_BOILER - Robot.shooter.LEEWAY
-			|| Robot.shooter.UltronRange() > Robot.shooter.DISTANCE_TO_BOILER + Robot.shooter.LEEWAY;
-	
-	
-		
+		return !shooterOn || Robot.shooter.UltronRange() < Robot.shooter.DISTANCE_TO_BOILER - Robot.shooter.LEEWAY
+				|| Robot.shooter.UltronRange() > Robot.shooter.DISTANCE_TO_BOILER + Robot.shooter.LEEWAY;
 	}
-
 }
--- src/org/usfirst/frc/team2537/robot/drive/ShooterSubsystem.java
+++ src/org/usfirst/frc/team2537/robot/drive/ShooterSubsystem.java
@@ -15,11 +15,13 @@ public class ShooterSubsystem extends Subsystem {
 	public static final int SPEED = 1;
 	public static final int LEEWAY = 1;
 	public static final int DISTANCE_TO_BOILER = 10;
-	private static Ultrasonic ultrasonic; 
-	
+	private static Ultrasonic ultrasonic_to_boiler; 
+	/**
+	 * Constructor that sets up Ultrasonic by setting Automatic Mode to true
+	 */
 	public ShooterSubsystem() {
-		ultrasonic = new Ultrasonic(Ports.ULTRASONIC_INPUT, Ports.ULTRASONIC_OUTPUT); //TODO Change ports to variables in Ports.jav
-		ultrasonic.setAutomaticMode(true);
+		ultrasonic_to_boiler = new Ultrasonic(Ports.ULTRASONIC_INPUT, Ports.ULTRASONIC_OUTPUT); //TODO Change ports to variables in Ports.jav
+		ultrasonic_to_boiler.setAutomaticMode(true);
 	}
 
 	@Override
@@ -27,10 +29,16 @@ public class ShooterSubsystem extends Subsystem {
 
 
 	}
+	/**
+	 * Sets Talon @param t to speed @param speed
+	 */
 	public void setFlySpeed(Talon t,double speed){
 		t.set(speed);
 		
 	}
+	/**
+	 * Sets all four flywheels to speed SPEED
+	 */
 	public void FlyOn(){
 		
 		setFlySpeed(BackLeftFly, SPEED);
@@ -38,16 +46,25 @@ public class ShooterSubsystem extends Subsystem {
 		setFlySpeed(BackRightFly, SPEED);
 		setFlySpeed(FrontRightFly, SPEED);
 	}
+	/**
+	 * Sets All four Flywheels to speed 0
+	 */
 	public void FlyOff(){
 		setFlySpeed(BackLeftFly, 0);
 		setFlySpeed(FrontLeftFly,0);
 		setFlySpeed(BackRightFly,0);
 		setFlySpeed(FrontRightFly,0);
 	}
-
+	/**
+	 * 
+	 * @return the distance of the ultrasonic on the robot to the boiler
+	 */
 	public double UltronRange(){
-		return  ultrasonic.getRangeInches();
+		return  ultrasonic_to_boiler.getRangeInches();
 	}
+	/**
+	 * Registers Buttons
+	 */
 	public void registerButtons(){
 		HumanInput.registerWhenPressedCommand(HumanInput.shooterFire, new ShooterCommand(true));
 		HumanInput.registerWhenPressedCommand(HumanInput.shooterOff, new ShooterCommand(false));
