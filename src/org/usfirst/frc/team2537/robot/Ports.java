57dfc51d3b37ed1e7e50d335d5626f1425764b53
Alex Taber <alex@ataber.pw>
2017-02-02 23:14:30 -0500

    Add todo; remove temp

--- src/org/usfirst/frc/team2537/robot/Ports.java
+++ src/org/usfirst/frc/team2537/robot/Ports.java
@@ -1,13 +1,15 @@
 package org.usfirst.frc.team2537.robot;
 
 public class Ports {
+	//TODO: Change ports to line up with actual ports
+	
 	// Drive Talon Ports
 	public static final int FRONT_LEFT_MOTOR = 1, FRONT_RIGHT_MOTOR = 2, BACK_LEFT_MOTOR = 3, BACK_RIGHT_MOTOR = 4;
 
 	// Mechanism Talon Ports
 	public static final int CLIMBER_MOTOR_ONE = 5, CLIMBER_MOTOR_TWO = 6;
 
-	public static final int SLOW_SHOOTER = 1, FAST_SHOOTER = 2; // temp
+	public static final int SLOW_SHOOTER = 1, FAST_SHOOTER = 2;
 
 	// Joystick and XBox Ports
 	public static final int LEFT_JOYSTICK = 0, RIGHT_JOYSTICK = 1, XBOX = 2;
@@ -15,7 +17,7 @@ public class Ports {
 	// XBox Buttons
 	public static final int CLIMBER_ACTIVATOR_BUTTON = 2, CLIMBER_KILL_SWITCH = 3;
 
-	public static final int SHOOTER_ON_BUTTON = 1, SHOOTER_OFF_BUTTON = 4; // temp
+	public static final int SHOOTER_ON_BUTTON = 1, SHOOTER_OFF_BUTTON = 4;
 
 	// Sensor ports
 	public static final int ULTRASONIC_TRIGGER = 2, ULTRASONIC_ECHO = 3;
