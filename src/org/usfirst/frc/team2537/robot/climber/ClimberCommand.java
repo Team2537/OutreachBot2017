37735c1200fd7b11382388834a98f781791cffe7
Alex Taber <alexftaber@gmail.com>
2017-03-08 18:35:39 -0500

    Changed Climber Speed

--- src/org/usfirst/frc/team2537/robot/climber/ClimberCommand.java
+++ src/org/usfirst/frc/team2537/robot/climber/ClimberCommand.java
@@ -23,7 +23,7 @@ public class ClimberCommand extends Command {
 	 */
 	@Override
 	protected void execute() {
-		Robot.climberSys.setClimberMotor(-.8);
+		Robot.climberSys.setClimberMotor(-1);
 	}
 
 	/**
